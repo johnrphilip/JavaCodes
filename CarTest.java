db2775f8256cf93f972ec88115bcffdd07b204fc
John <96958726+johnrphilip@users.noreply.github.com>
2023-04-21 10:02:39 +0100

    Initial commit

---
+++ CarTest.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class CarTest {
+
+	public static void main(String[] args) {
+	
+        ArrayList<Car> cars = new ArrayList<>();
+
+        // add some cars to the list
+        cars.add(new Car("001", "Toyota", "Corolla", 2010, 100000, 1.8, "Poor", 8000));
+        cars.add(new Car("002", "Honda", "Civic", 2012, 80000, 1.6, "Good", 9000));
+        cars.add(new Car("003", "Ford", "Mustang", 2015, 50000, 3.7, "Adequate", 15000));
+        cars.add(new Car("004", "Chevrolet", "Impala", 2018, 25000, 3.6, "Excellent", 12000));
+        cars.add(new Car("005", "Tesla", "Model S", 2019, 10000, 0.0, "Good", 45000));
+        cars.add(new Car("006", "Nissan", "Altima", 2016, 60000, 2.5, "Poor", 9500));
+        cars.add(new Car("007", "BMW", "X5", 2017, 40000, 3.0, "New", 27000));
+        cars.add(new Car("008", "Mercedes-Benz", "C-Class", 2015, 75000, 2.0, "Good", 18000));
+        cars.add(new Car("009", "Mercedes-Benz", "C-Class", 2015, 75000, 2.0, "Good", 18000));
+        cars.add(new Car("010", "Mercedes-Benz", "C-Class", 2015, 75000, 2.0, "Good", 18000));
+
+        // print the details of each car in the list
+      
+     
+        
+	}
+	
+}
+
+/*
+
+
+*/
